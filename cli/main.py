4e063ab2087588fcff4a07a0837e9a3cced91a0b
skandrigi <skandrigi@gmail.com>
2024-11-02 21:13:14 -0500

    make it half of screen

--- cli/main.py
+++ cli/main.py
@@ -5,6 +5,8 @@ class ScreenApp(App):
     def on_mount(self) -> None:
         self.screen.styles.background = "#161b2a"
         self.screen.styles.border = ("heavy", "white")
+        self.widget.styles.width = 50
+        
 
 
 if __name__ == "__main__":
