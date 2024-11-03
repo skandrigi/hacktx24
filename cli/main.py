30fbae2e35f0675b45eccc0d577b4cfb1fedd6da
skandrigi <skandrigi@gmail.com>
2024-11-02 21:53:20 -0500

    add files section

--- cli/main.py
+++ cli/main.py
@@ -3,22 +3,26 @@ from textual.widgets import Static
 
 class ScreenApp(App):
     def compose(self) -> ComposeResult:
-        self.widget = Static("hi")
-        self.gir = Static("hi1")
-        
+        self.widget = Static("<<< MERGR 🍒")
+        self.files = Static("FILES")
+
         yield self.widget
-        yield self.gir
+        yield self.files
 
     def on_mount(self) -> None:
         # Screen styles
         self.screen.styles.background = "#161b2a"
-        self.screen.styles.border = ("heavy", "white")
-        self.widget.styles.width = "2fr"
-        self.widget.styles.height = "3fr"
+        self.widget.styles.width = "3fr"
+        self.widget.styles.min_height = "10vh"
         self.widget.styles.background = "red"
-        self.gir.styles.width = "1fr"
-        self.gir.styles.height = "3fr"
-        self.gir.styles.background = "purple"
+        self.widget.styles.padding = 2
+        self.files.styles.border_left = ("dashed","#1C6FFF")
+        self.files.styles.border_right = ("dashed","#1C6FFF")
+        self.files.styles.border_top = ("double","#1C6FFF")
+        self.files.styles.border_bottom = ("double","#1C6FFF")
+        self.files.styles.border_title = "FILES"
+        self.files.styles.min_height= "90vh"
+        self.files.styles.min_width= "20vh"
 
 if __name__ == "__main__":
     app = ScreenApp()
