200d9d6cf9cc648c167115c104b360eba0c48d7b
skandrigi <skandrigi@gmail.com>
2024-11-02 21:56:05 -0500

    update border title

--- cli/main.py
+++ cli/main.py
@@ -4,7 +4,7 @@ from textual.widgets import Static
 class ScreenApp(App):
     def compose(self) -> ComposeResult:
         self.widget = Static("<<< MERGR 🍒")
-        self.files = Static("FILES")
+        self.files = Static("")
 
         yield self.widget
         yield self.files
@@ -13,16 +13,19 @@ class ScreenApp(App):
         # Screen styles
         self.screen.styles.background = "#161b2a"
         self.widget.styles.width = "3fr"
-        self.widget.styles.min_height = "10vh"
+        self.widget.styles.max_height = "10vh"
         self.widget.styles.background = "red"
         self.widget.styles.padding = 2
+
         self.files.styles.border_left = ("dashed","#1C6FFF")
         self.files.styles.border_right = ("dashed","#1C6FFF")
         self.files.styles.border_top = ("double","#1C6FFF")
         self.files.styles.border_bottom = ("double","#1C6FFF")
-        self.files.styles.border_title = "FILES"
-        self.files.styles.min_height= "90vh"
-        self.files.styles.min_width= "20vh"
+        self.files.border_title = "FILES"
+        self.files.border_title_align = "center"
+        self.files.border_title_color = "white"
+        self.files.styles.max_height= "90vh"
+        self.files.styles.max_width= "20vh"
 
 if __name__ == "__main__":
     app = ScreenApp()
