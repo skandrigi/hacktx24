523e694618ff98cc19025a5074ad65ae3a9ce1fe
skandrigi <skandrigi@gmail.com>
2024-11-02 22:04:21 -0500

    update title margin

--- cli/main.py
+++ cli/main.py
@@ -12,25 +12,21 @@ class ScreenApp(App):
 
     def on_mount(self) -> None:
         # Screen styles
-        self.screen.styles.background = "#161b2a"
-
-        # Widget styles
         self.widget.styles.width = "100%"
         self.widget.styles.height = "auto"  # Adjust height to auto to fit content
         self.widget.styles.background = "red"
-        self.widget.styles.padding = 2
-        self.widget.styles.text_align = "center"  # Ensure text is centered
+        self.widget.styles.margin = 1
+        self.widget.styles.text_align = "left"  # Ensure text is centered
 
-        # Files styles
-        self.files.styles.border_left = ("dashed", "#1C6FFF")
-        self.files.styles.border_right = ("dashed", "#1C6FFF")
-        self.files.styles.border_top = ("double", "#1C6FFF")
-        self.files.styles.border_bottom = ("double", "#1C6FFF")
-        self.files.styles.border_title = "FILES"
-        self.files.styles.border_title_align = "center"
+        self.files.styles.border_left = ("dashed","#1C6FFF")
+        self.files.styles.border_right = ("dashed","#1C6FFF")
+        self.files.styles.border_top = ("double","#1C6FFF")
+        self.files.styles.border_bottom = ("double","#1C6FFF")
+        self.files.border_title = "FILES"
+        self.files.border_title_align = "center"
         self.files.styles.border_title_color = "white"
-        self.files.styles.height = "90vh"
-        self.files.styles.width = "20vw"
+        self.files.styles.height= "90vh"
+        self.files.styles.width= "20vw"
 
 if __name__ == "__main__":
     app = ScreenApp()
