a3b64aadd0ad4c257b6c293d39a0a113e1b45e9a
liannie3 <liannie@tamu.edu>
2024-11-03 01:27:15 -0500

    adjust colors

--- cli/main.py
+++ cli/main.py
@@ -24,13 +24,13 @@ class ScreenApp(App):
 
     def on_mount(self) -> None:
         # Screen styles
-        self.screen.styles.background = "#2B263B"
+        self.screen.styles.background = "#28233B"
         self.widget.styles.width = "100%"
         self.widget.styles.height = "auto"  # Adjust height to auto to fit content
         self.widget.styles.margin = 2
         self.widget.styles.text_align = "left"  # Ensure text is centered
 
-        self.files.styles.background = "#2B263B"
+        self.files.styles.background = "#28233B"
         self.files.styles.border_left = ("dashed","#1C6FFF")
         self.files.styles.border_right = ("dashed","#1C6FFF")
         self.files.styles.border_top = ("double","#1C6FFF")
@@ -42,25 +42,25 @@ class ScreenApp(App):
         self.files.styles.width= "17vw"
         self.files.styles.margin = 2
         
-        code_title = Text("C", style="white")
-        code_title.append("\U00002B24", style="FFABAB") 
+        code_title = Text("", style="white")
+        code_title.append("C", style="white")
+        code_title.append("\U00002B24", style="#FFABAB") 
         code_title.append("DE", style="white")
         
-        self.code.styles.background = "#2B263B"
+        
+        self.code.styles.background = "#28233B"
         self.code.styles.border_left = ("dashed","#1C6FFF")
         self.code.styles.border_right = ("dashed","#1C6FFF")
         self.code.styles.border_top = ("double","#1C6FFF")
         self.code.styles.border_bottom = ("double","#1C6FFF")
         self.code.border_title = code_title
         self.code.border_title_align = "left"
-        self.code.styles.border_title_color = "white"
         self.code.styles.height= "76vh"
         self.code.styles.width= "37vw"
         self.code.styles.margin = 2
 
         comment_title = Text("C", style="white")
-        comment_title.append("\U00002B24", style="FFABAB") 
-        comment_title.append("\U000015E3", style="FFABAB") 
+        comment_title.append("\U00002B24", style="#FFABAB") 
         comment_title.append("MMENTS", style="white")
         self.code.styles.overflow = "auto" 
         
@@ -76,7 +76,7 @@ class ScreenApp(App):
         self.comment.styles.margin = 2
 
         command_title = Text("C", style="white")
-        command_title.append("\U00002B24", style="FFABAB") 
+        command_title.append("\U00002B24", style="#FFABAB") 
         command_title.append("MMAND", style="white")
 
         self.command.styles.border_left = ("dashed","#1C6FFF")
@@ -85,7 +85,6 @@ class ScreenApp(App):
         self.command.styles.border_bottom = ("double","#1C6FFF")
         self.command.border_title = command_title 
         self.command.border_title_align = "left"
-        self.command.styles.border_title_color = "white"
         self.command.styles.height= "13vh"
         self.command.styles.width= "75vw"
         self.command.styles.margin = 2
