9b2eb1e2baaf35dc91ab96f025a9329a68933d99
skandrigi <skandrigi@gmail.com>
2024-11-02 22:00:41 -0500

    updated cli styling with files

--- cli/main.py
+++ cli/main.py
@@ -1,6 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Static
 
+
 class ScreenApp(App):
     def compose(self) -> ComposeResult:
         self.widget = Static("<<< MERGR 🍒")
@@ -12,20 +13,24 @@ class ScreenApp(App):
     def on_mount(self) -> None:
         # Screen styles
         self.screen.styles.background = "#161b2a"
-        self.widget.styles.width = "3fr"
-        self.widget.styles.max_height = "10vh"
+
+        # Widget styles
+        self.widget.styles.width = "100%"
+        self.widget.styles.height = "auto"  # Adjust height to auto to fit content
         self.widget.styles.background = "red"
         self.widget.styles.padding = 2
+        self.widget.styles.text_align = "center"  # Ensure text is centered
 
-        self.files.styles.border_left = ("dashed","#1C6FFF")
-        self.files.styles.border_right = ("dashed","#1C6FFF")
-        self.files.styles.border_top = ("double","#1C6FFF")
-        self.files.styles.border_bottom = ("double","#1C6FFF")
-        self.files.border_title = "FILES"
-        self.files.border_title_align = "center"
-        self.files.border_title_color = "white"
-        self.files.styles.max_height= "90vh"
-        self.files.styles.max_width= "20vh"
+        # Files styles
+        self.files.styles.border_left = ("dashed", "#1C6FFF")
+        self.files.styles.border_right = ("dashed", "#1C6FFF")
+        self.files.styles.border_top = ("double", "#1C6FFF")
+        self.files.styles.border_bottom = ("double", "#1C6FFF")
+        self.files.styles.border_title = "FILES"
+        self.files.styles.border_title_align = "center"
+        self.files.styles.border_title_color = "white"
+        self.files.styles.height = "90vh"
+        self.files.styles.width = "20vw"
 
 if __name__ == "__main__":
     app = ScreenApp()
